762a7c8f60271b5667d9ddbf447abbcb30bca373
Попов Ярослав Clemson <popov.yarik.popov@gmail.com>
2024-03-31 17:52:47 +0300

    lr10

--- obj/Debug/net8.0/Libra.AssemblyInfo.cs
+++ obj/Debug/net8.0/Libra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Libra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d78daeaef195689bd3c6c2c9a81f8862ecd4801")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aef6a44f9959cc696cb767b719a7a3cca9a10d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Libra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Libra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
